b0538afd581586385226ca6fcf56a8683294a44f
Kim <elektroschmock78@googlemail.com>
2016-06-30 19:37:45 +0200

    Adopt changes made in Quasar 1.5
    * import changes from Quasar 1.5
    * create subversion 1.5.1

--- installer.php
+++ installer.php
@@ -2,7 +2,7 @@
 /**
  * @package   gantry
  * @subpackage core
- * @version   1.4 February 20, 2015
+ * @version   1.5.1 June 30, 2016
  * @author    RocketTheme http://www.rockettheme.com
  * @copyright Copyright (C) 2007 - 2015 RocketTheme, LLC
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
